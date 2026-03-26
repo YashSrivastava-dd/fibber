f7929ce633103daf5275ff9f3ed0fb06d88fad98
YashSrivastava-dd <Yash@agvahealthtech.com>
2026-03-26 11:42:02 +0530

    fb tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ const playfair = Playfair_Display({
 
 const SITE_URL = 'https://fiberisefit.com'
 const OG_IMAGE = `${SITE_URL}/icons/I%20Mark%20-%20BC%2001.png`
+const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || '1494405295572983'
 
 export const metadata: Metadata = {
   title: 'Fiberise Fit',
@@ -76,6 +77,30 @@ export default function RootLayout({
             gtag('config', 'AW-17953867063');
           `}
         </Script>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         {/* Zoho SalesIQ chat widget */}
         {/* <Script
           id="zoho-salesiq-ready"e
